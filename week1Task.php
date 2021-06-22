c3f9dc27f97950539b5f4ab6f2d6c94159710ebb
Ismail366 <86307959+Ismail366@users.noreply.github.com>
2021-06-22 18:10:59 +0100

    Update week1Task.php

--- week1Task.php
+++ week1Task.php
@@ -4,6 +4,16 @@ function range($start, $end){
         echo $i;
     }
 }
+function sum_array($numbers){
+    $num_array = array($numbers);
+    $sum = 0;
+    for($i=0,$i<count($num_array) $i++){
+        foreach($num_array as $number){
+            $sum = $sum + $number;
+        }
+    }
+    return $sum
+}
 
 
 ?>
