7b558bd8cfd4a5798bcdd15641f33f86c29424b3
Ismail366 <86307959+Ismail366@users.noreply.github.com>
2021-06-22 15:48:55 +0100

    Create secondTask.php

---
+++ secondTask.php
@@ -0,0 +1,15 @@
+<?php
+function sum_array($numbers){
+    $num_array = array($numbers);
+    $sum = 0;
+    for($i=0,$i<count($num_array) $i++){
+        foreach($num_array as $number){
+            $sum = $sum + $number;
+        }
+    }
+    return $sum
+}
+
+
+
+?>
