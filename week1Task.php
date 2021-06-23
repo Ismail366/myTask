c1f9711979dc9ab1866ace5f88b92cfb6d49d128
Ismail366 <86307959+Ismail366@users.noreply.github.com>
2021-06-24 00:06:48 +0100

    Update week1Task.php

--- week1Task.php
+++ week1Task.php
@@ -3,7 +3,7 @@
 function frange($start, $end){
     $num_array = range($start,$end);
     foreach($num_array as $num){
-        echo $num."<br>"
+        echo $num."<br>";
     }
 }
 /*array sum function*/
@@ -17,8 +17,8 @@ $sum=0;
     return $sum;   
 }
 
-frange(1,2);
+frange(1,5);//first function call
 echo "<br>";
-echo sum_array(1,2,3,4);
+echo sum_array(1,2,3,4);//second function call
 
 ?>
