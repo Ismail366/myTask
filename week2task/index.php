3b032a719fb91e58fed4fcb05aca672ef1a19f67
Ismail366 <86307959+Ismail366@users.noreply.github.com>
2021-06-30 15:21:26 +0100

    Create index.php

---
+++ week2task/index.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>   
+<?php
+session_start();
+?>
+<head>
+    <title></title>
+</head>
+<body>
+  <form action="index.php" method="POST">
+      <h1>Registration Form</h1>
+      <br>
+      Firt Name:<input type="text"name="fname"placeholder="Enter your First Name"required>
+      <br>
+      Surname:<input type="text" name="sname"placeholder="Enter your Surname"required>
+      <br>
+      Email:<input type="email"name="email"placeholder="E-mail"required>
+      <br>
+      Password:<input type="password" name="password"placeholder="set a password"required>
+      <br>
+      <input type= "submit" name = "submit"value ="register">
+      <br>
+      <hr>
+  </form>
+  <?php
+  if(isset($_POST["submit"])){
+      echo "You have successfully registered!";
+      echo "<br>";
+      
+  }
+$email = $_POST["email"];
+$password = $_POST["password"];
+$_SESSION["email"]= $email;
+$_SESSION["password"] = $password;
+ 
+?>
+  <button><a href="login.php">Login</a></button>
+  
+</body>
+</html>
